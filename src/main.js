6e6786cfd4895f873fe60aa1b15cae57efdeba93
wangmingda <wang521763@163.com>
2020-02-29 15:42:23 +0800

    初始化项目-准备目录-整理资源

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import Element from 'element-ui' // 引入模块
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import '@/styles/index.less' // 引入样式=>初始化工作搞定
 Vue.config.productionTip = false
 Vue.use(Element) // 全局注册element模块，所有的位置都可以使用组件 这样就能为所欲为了
 new Vue({
