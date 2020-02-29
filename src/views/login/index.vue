943eebc3296bb94f40d294c74f8cb58079ffc44b
wangmingda <wang521763@163.com>
2020-02-29 17:06:36 +0800

    登录模块-页面布局及样式

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,6 +1,33 @@
 <template>
-  <div>
-      <h1>我是登陆页</h1>
+  <div class="login">
+      <!-- 登陆表单 -->
+      <el-card class="login-card">
+        <!-- 表单内容 -->
+        <!-- 1.头部logo部分 -->
+        <div class="logo">
+        <img src="../../assets/img/logo_index.png" alt="">
+        </div>
+        <!-- 2.表单 -->
+        <el-form style="margin-top:20px">
+          <!-- 表单容器 -->
+          <el-form-item>
+            <!-- 2.1填写手机号表单域 -->
+            <el-input placeholder="请输入手机号"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <!-- 2.2填写手机验证码表单域 -->
+            <el-input style='width:60%' placeholder="请输入验证码"></el-input>
+            <el-button style='float:right;width:35%' plain="">发送验证码</el-button>
+          </el-form-item>
+          <el-form-item>
+            <!-- 2.3服务条款表单域 -->
+            <el-checkbox>我已经阅读同意用户协议和服务条款</el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button style='width:100%;' type="primary" plain="">登陆</el-button>
+          </el-form-item>
+        </el-form>
+      </el-card>
   </div>
 </template>
 
@@ -10,6 +37,25 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang='less' scoped>
+// 给标签添加了scoped属性，当前样式就默认只会对本页面生效
+// 想用less写法，必须给style标签添加lang属性
+  .login {
+    background-image: url('../../assets/img/login_bg.jpg');
+    height: 100vh; // 当前屏幕可视区域分为100份
+    background-size: cover;
+    display: flex; // 采用flex布局
+    justify-content: center; // 水平居中
+    align-items: center; // 垂直居中
+  .login-card {
+    width: 440px;
+    height: 340px;
+    .logo {
+      text-align: center;
+      img {
+        height: 40px;
+      }
+    }
+  }
+  }
 </style>
