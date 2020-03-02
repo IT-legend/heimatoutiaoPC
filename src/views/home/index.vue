dc61110c29b178c5441b068d4af9402cee115b3b
wangmingda <wang521763@163.com>
2020-03-02 18:50:35 +0800

    完成主页整体布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <div>
-      <h1>我是主页</h1>
-  </div>
+<!-- 必须在最外层放置一个container容器 因为所有小容器必须放在这个大容器中 -->
+  <el-container>
+      <!-- 左侧 -->
+      <el-aside>
+        左侧导航区域
+      </el-aside>
+      <!-- 右侧 -->
+      <el-container>
+        <!-- 右侧上部分 -->
+        <el-header>顶部操作栏</el-header>
+        <!-- 右侧下部分 -->
+        <el-main>
+          主要区域
+          <!-- 这里应该放置二级路由 -->
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+  </el-container>
 </template>
 
 <script>
