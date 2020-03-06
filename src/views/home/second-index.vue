b1925b964db98c9cb04c6fbcd931277ef4a8544f
wangmingda <wang521763@163.com>
2020-03-06 14:22:05 +0800

    素材列表-基本布局完成

--- src/views/home/second-index.vue
+++ src/views/home/second-index.vue
@@ -17,9 +17,9 @@ export default {
       list:
       [
         'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3663511007,1366474142&fm=26&gp=0.jpg',
-        'https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=4140202155,2103115811&fm=26&gp=0.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583477232011&di=68823ee4cb24d15927669335249ef89d&imgtype=0&src=http%3A%2F%2Fpic1.win4000.com%2Fwallpaper%2F3%2F5581156b05639.jpg',
         'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=83118165,4156422393&fm=26&gp=0.jpg',
-        'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=523672346,1836782431&fm=26&gp=0.jpg',
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583477354789&di=e907f58dd6f238825c5c6493be653317&imgtype=0&src=http%3A%2F%2Fdesk.fd.zol-img.com.cn%2Ft_s960x600c5%2Fg5%2FM00%2F02%2F02%2FChMkJlbKxkGIEjWxAAatrBKE4RIAALHigBlp5sABq3E101.jpg',
         'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1583256058201&di=6d347ea46bb7fe7c483b103f12297731&imgtype=0&src=http%3A%2F%2Fwww.netbian.com%2Fd%2Ffile%2F20140511%2F2f42b589066cb7baba9f8a3ab820dd45.jpg'
       ]
     }
--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -8,12 +8,28 @@
           素材管理
       </template>
       </bread-crumb>
+      <!-- 放置标签页 -->
+      <el-tabs v-model="activeName">
+          <!-- 子选项 -->
+          <el-tab-pane label="全部素材" name="all">
+              <!-- 子选项内容 -->
+              all
+          </el-tab-pane>
+          <el-tab-pane label="收藏素材" name="collect">
+              <!-- 子选项内容 -->
+              like
+          </el-tab-pane>
+      </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all' // 当前默认选中的页签
+    }
+  }
 }
 </script>
 
