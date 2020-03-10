bd0e0d2255f0c51019bf37e0fa373076298e5eab
wangmingda <wang521763@163.com>
2020-03-11 00:49:56 +0800

    页面进度条的应用

--- src/permission/index.js
+++ src/permission/index.js
@@ -1,7 +1,13 @@
 // 专门处理路由权限的问题
 import router from '@/router'
+import progress from 'nprogress' // 引入进度条包
+import 'nprogress/nprogress.css' // 引入进度条样式文件
 // 注册全局前置守卫
 router.beforeEach(function (to, from, next) {
+  // 在路由发生变化时
+  // 开启进度条
+  progress.start()
+
   // next() //表示放过
   // next(false) //表示拦截
   // next(新地址) // 表示跳转到新页面
@@ -19,4 +25,10 @@ router.beforeEach(function (to, from, next) {
     //   不是home开头，放过
     next()
   }
+})
+// 在全局的后置守卫的位置关闭，此时页面已经完成跳转
+router.afterEach(() => {
+  // setTimeout(() => progress.done(), 1000)
+  // 关闭进度条
+  progress.done()
 })
