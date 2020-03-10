55b33789c15b02ad7603f349b371af5b1ba2b1f4
wangmingda <wang521763@163.com>
2020-03-10 23:53:33 +0800

    账户信息-调接口获取数据加载给表单

--- src/views/account/index.vue
+++ src/views/account/index.vue
@@ -8,21 +8,21 @@
             <el-form label-width="120px">
                 <!-- 第一行表单域 用户名-->
                 <el-form-item label="用户名">
-                    <el-input style="width:30%"></el-input>
+                    <el-input style="width:30%" v-model="formData.name"></el-input>
                 </el-form-item>
                 <!-- 第二行表单域 用户简介-->
                 <el-form-item label="用户简介">
-                    <el-input style="width:30%"></el-input>
+                    <el-input style="width:30%" v-model="formData.intro"></el-input>
 
                 </el-form-item>
                 <!-- 第三行表单域 用户邮箱-->
                 <el-form-item label="用户邮箱">
-                    <el-input style="width:30%"></el-input>
+                    <el-input style="width:30%" v-model="formData.email"></el-input>
 
                 </el-form-item>
-                <!-- 第四行表单域 手机号码能看不能改-->
+                <!-- 第四行表单域 手机号码能看不能改，设置disabled属性即可-->
                 <el-form-item label="手机号码">
-                    <el-input style="width:30%" disabled=""></el-input>
+                    <el-input style="width:30%" disabled="" v-model="formData.mobile"></el-input>
 
                 </el-form-item>
                 <!-- 第五行表单域 功能按钮-->
@@ -37,7 +37,33 @@
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      // 定义个人信息数据
+      formData: {
+        name: '',
+        intro: '',
+        photo: '',
+        email: '',
+        mobile: ''
+      }
+    }
+  },
+  methods: {
+    //   1-定义获取用户信息方法
+    getUserInfo () {
+      this.$axios({
+        url: '/user/profile'
+      }).then(result => {
+        //   将获取到的数据赋值给我们的表单数据
+        this.formData = result.data
+      })
+    }
+  },
+  created () {
+    //   调用获取数据方法
+    this.getUserInfo()
+  }
 }
 </script>
 
